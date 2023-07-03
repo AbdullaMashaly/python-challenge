2fc75b95dbaa23bd9469686072963f76de6faedd
AbdullaMashaly <abdullamashaly11@gmail.com>
2023-07-02 22:25:01 -0400

    Pybank commit

---
+++ PyBank/main.py
@@ -0,0 +1,82 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder
+budget_csv = os.path.join('Resources', 'budget_data.csv')
+
+# list to store data
+change = []
+
+# Read in the CSV file
+with open( budget_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+    data = list(csvreader)
+
+    # to calculate the total number of months
+    total_months = len(data)
+
+    # to calculate the net total amount of "Profit/Losses" over the entire period
+    net_total = sum(int(row[1]) for row in data)
+
+    # list to store data
+    changes = []
+    last_profit_loss = int(data[0][1])
+    
+    # to calculate change over each month
+    for row in data[1:]:
+        current_profit_loss = int(row[1])
+        change = current_profit_loss - last_profit_loss
+        changes.append(change)
+        last_profit_loss = current_profit_loss
+
+
+    # to calculate avergae change
+    average_change = round(sum(changes) / len(changes), 2)
+
+    # to calculate greateest increase in profits
+    maximum_change_increase = max(changes)
+    max_increase_index = changes.index(maximum_change_increase)
+    max_increase_date = data[max_increase_index + 1][0]
+    
+    
+    # to calculate greatest decrease in profits 
+    maximum_change_decrease = min(changes)
+    max_decrease_index = changes.index(maximum_change_decrease)
+    max_decrease_date = data[max_decrease_index + 1][0]
+
+print(f"""
+Financial Analysis
+
+----------------------------
+
+Total Months: {total_months}
+Total: ${net_total}
+Average Change: ${average_change}
+Greatest Increase in Profits: {max_increase_date} (${maximum_change_increase})
+Greatest Decrease in Profits: {max_decrease_date} (${maximum_change_decrease})
+""")
+
+
+
+# Specify the file to write to
+output_path = os.path.join( "analysis", "analysis.txt")
+
+# Open the file using "write" mode.
+with open(output_path, 'w') as txtfile:
+    txtfile.write(f"""
+Financial Analysis
+
+----------------------------
+
+Total Months: {total_months}
+Total: ${net_total}
+Average Change: ${average_change}
+Greatest Increase in Profits: {max_increase_date} (${maximum_change_increase})
+Greatest Decrease in Profits: {max_decrease_date} (${maximum_change_decrease})
+""")
+
+print("Analysis results have been saved to analysis.txt.")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-challenge
 Module challenge 3
+Source code:
+To calculate the number of months using len https://www.w3resource.com/python-exercises/modules/python-module-csv-exercise-2.php
+To refer to a specific field of a CSV file https://stackoverflow.com/questions/5757743/how-can-i-get-a-specific-field-of-a-csv-file
+Sum Comprehension https://docs.sourcery.ai/Reference/Python/Default-Rules/sum-comprehension/
+Save a text file to a certain directory and adding text to it https://stackoverflow.com/questions/8024248/telling-python-to-save-a-txt-file-to-a-certain-directory-on-windows-and-mac
